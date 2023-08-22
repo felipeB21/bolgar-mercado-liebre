717174332e1afcaae6db077b514d19fe2109cbdd
FelipeB21 <bolgarfelipe@gmail.com>
2023-08-21 21:10:21 -0300

    port process

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ const path = require('path');
 
 app.use(express.static(path.join(__dirname, "../public")));
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server is woriking in PORT: ${PORT}`);
 });
